d2dc96fc8426c4bf4f9791ec84098fc7861ad1c5
remi <remi.lanneau.unique@gmail.com>
2020-03-20 09:02:08 +0100

    ModuleServ 0.1.1

--- Src/ModuleServer/ServerTest.py
+++ Src/ModuleServer/ServerTest.py
@@ -1,5 +1,10 @@
-from http.server import HTTPserver, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        pass
+        to_send = "Ca marche"
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(to_send, "utf-8"))
+serv = HTTPServer(('localhost', 8000), Server)
+serv.serve_forever()
