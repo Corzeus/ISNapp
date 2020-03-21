d49a7d65dce951d891eecc9b60e5244d028854f9
remi <remi.lanneau.unique@gmail.com>
2020-03-21 16:15:00 +0100

    Update ServerTest.py

--- Src/ModuleServer/ServerTest.py
+++ Src/ModuleServer/ServerTest.py
@@ -1,10 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import cgi
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        to_send = "Ca marche"
+        to_send = "Ca marche" #il faut modifier cette var par le contenu des pages webs pour pouvoir les afficher
         self.send_response(200)
         self.end_headers()
         self.wfile.write(bytes(to_send, "utf-8"))
+
+    def do_POST(self):
+        pass
+        #recevoir les informations de modification des plans, agir en conséquence puis renvoyer la page d'acceuil
+
 serv = HTTPServer(('localhost', 8000), Server)
 serv.serve_forever()
